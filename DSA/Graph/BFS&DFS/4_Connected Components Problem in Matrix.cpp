1941a5d0054c9c259ca2362fc52611e05f02e5e4
Shrey123ansh <shreyann56sh@gmail.com>
2024-05-24 13:20:01 +0530

    commit: graph basics

---
+++ DSA/Graph/BFS&DFS/4_Connected Components Problem in Matrix.cpp
@@ -0,0 +1,43 @@
+int findCircleNum(vector<vector<int>> &isConnected)
+{
+    int n = isConnected.size();
+    vector<int> adj[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (isConnected[i][j] == 1 && i != j)
+            {
+                adj[i].push_back(j);
+                adj[j].push_back(i);
+            }
+        }
+    }
+
+    vector<int> vis(n, 0);
+    int ans = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (vis[i] == 0)
+        {
+            ans++;
+            dfs(i, vis, adj);
+        }
+    }
+    return ans;
+}
+
+void dfs(int node, vector<int> &vis, vector<int> adj[])
+{
+    vis[node] = 1;
+
+    for (auto it : adj[node])
+    {
+        if (!vis[it])
+        {
+            dfs(it, vis, adj);
+        }
+    }
+}
