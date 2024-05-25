635d2d78d39aed9a3fa998251e7b6e5dbf3307ab
Shrey123ansh <shreyann56sh@gmail.com>
2024-05-25 16:24:30 +0530

    "Added iterative solutions for inorder, postorder, and preorder traversals of binary trees."

--- DSA/Trees/Easy/Post-order Traversal of Binary Tree.cpp
+++ DSA/Trees/Easy/Post-order Traversal of Binary Tree.cpp
@@ -16,3 +16,95 @@ void preorder(TreeNode *root, vector<int> &arr)
     preorder(root->right, arr);
     arr.push_back(root->val);
 }
+
+/////////////////OR////////////////////////
+
+vector<int> postorderTraversal(TreeNode *root)
+{
+    vector<int> nodes;
+    stack<TreeNode *> todo;
+    TreeNode *last = NULL;
+    while (root || !todo.empty())
+    {
+        if (root)
+        {
+            todo.push(root);
+            root = root->left;
+        }
+        else
+        {
+            TreeNode *node = todo.top();
+            if (node->right && last != node->right)
+            {
+                root = node->right;
+            }
+            else
+            {
+                nodes.push_back(node->val);
+                last = node;
+                todo.pop();
+            }
+        }
+    }
+    return nodes;
+}
+
+/////////////////OR////////////////////////
+
+vector<int> postOrder(Node *root)
+{
+    // Vector to store
+    // postorder traversal
+    vector<int> postorder;
+
+    // If the tree is empty,
+    // return an empty traversal
+    if (root == NULL)
+    {
+        return postorder;
+    }
+
+    // Two stacks for
+    // iterative traversal
+    stack<Node *> st1, st2;
+
+    // Push the root node
+    // onto the first stack
+    st1.push(root);
+
+    // Iterative traversal to populate
+    // st2 with nodes in postorder
+    while (!st1.empty())
+    {
+        // Get the top node from st1
+        root = st1.top();
+        st1.pop();
+
+        // Push the node onto st2
+        st2.push(root);
+
+        // Push left child onto st1 if exists
+        if (root->left != NULL)
+        {
+            st1.push(root->left);
+        }
+
+        // Push right child onto st1 if exists
+        if (root->right != NULL)
+        {
+            st1.push(root->right);
+        }
+    }
+
+    // Populate the postorder traversal
+    // vector by popping st2
+    while (!st2.empty())
+    {
+        postorder.push_back(st2.top()->data);
+        st2.pop();
+    }
+
+    // Return the
+    // postorder traversal
+    return postorder;
+}
--- DSA/Trees/Easy/Preorder Traversal of Binary Tree.cpp
+++ DSA/Trees/Easy/Preorder Traversal of Binary Tree.cpp
@@ -16,3 +16,28 @@ void preorder(TreeNode *root, vector<int> &arr)
     preorder(root->left, arr);
     preorder(root->right, arr);
 }
+
+///////////OR////////////
+
+vector<int> preorderTraversal(TreeNode *root)
+{
+
+    vector<int> ans;
+    if (!root)
+        return ans;
+
+    stack<TreeNode *> st;
+    st.push(root);
+    while (!st.empty())
+    {
+        TreeNode *node = st.top();
+        st.pop();
+
+        ans.push_back(node->val);
+        if (node->right)
+            st.push(node->right);
+        if (node->left)
+            st.push(node->left);
+    }
+    return ans;
+}
