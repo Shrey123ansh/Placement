fdb36702799d3651f30a137987e166e351dc4354
Shreyansh Singhal <103640320+Shrey123ansh@users.noreply.github.com>
2024-07-10 16:09:42 +0530

    Update 1_N meetings in one room.cpp

--- DSA/Greedy/Medium/1_N meetings in one room.cpp
+++ DSA/Greedy/Medium/1_N meetings in one room.cpp
@@ -1,3 +1,10 @@
+Input: n = 6, start[] = {1,3,0,5,8,5}, end[] =  {2,4,6,7,9,9}
+Output: 4
+Explanation: Maximum four meetings can be held with given start and end timings. The meetings are - (1, 2),(3, 4), (5,7) and (8,9)
+Input: n = 3, start[] = {10, 12, 20}, end[] = {20, 25, 30}
+Output: 1
+Explanation: Only one meetings can be held with given start and end timings.
+
 struct meeting
 {
     int start;
