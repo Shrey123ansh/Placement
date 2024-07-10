c58ae8903a11e7be501365fcd692eacc77afcc71
Shreyansh Singhal <103640320+Shrey123ansh@users.noreply.github.com>
2024-07-10 16:46:04 +0530

    Update 5_Replace each array element by its corresponding rank.cpp

--- DSA/Heaps/Medium/5_Replace each array element by its corresponding rank.cpp
+++ DSA/Heaps/Medium/5_Replace each array element by its corresponding rank.cpp
@@ -1,4 +1,16 @@
-vector<int> replaceWithRank(vector<int> &arr, int N)
+Input:
+N = 6
+arr = [20, 15, 26, 2, 98, 6]
+Output:
+4, 3, 5, 1, 6, 2
+Explanation:
+After sorting, array becomes {2,6,15,20,26,98}
+Rank(2) = 1 (at index 0) 
+Rank(6) = 2 (at index 1) 
+Rank(15) = 3 (at index 2) 
+Rank(20) = 4 (at index 3) and so on..
+    
+    vector<int> replaceWithRank(vector<int> &arr, int N)
 {
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
