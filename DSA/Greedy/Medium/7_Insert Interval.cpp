99e167337f2b2ee37cee304a52e85f1d2c50ec39
Shreyansh Singhal <103640320+Shrey123ansh@users.noreply.github.com>
2024-07-10 16:12:02 +0530

    Update 7_Insert Interval.cpp

--- DSA/Greedy/Medium/7_Insert Interval.cpp
+++ DSA/Greedy/Medium/7_Insert Interval.cpp
@@ -1,4 +1,14 @@
-vector<vector<int>> insert(vector<vector<int>> &intervals, vector<int> &newInterval)
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+Example 2:
+
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+    
+    vector<vector<int>> insert(vector<vector<int>> &intervals, vector<int> &newInterval)
 {
     vector<vector<int>> result;
     int i = 0;
