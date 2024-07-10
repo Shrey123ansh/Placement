366bc945dcf310344a06be672566c9748c2a64ea
Shreyansh Singhal <103640320+Shrey123ansh@users.noreply.github.com>
2024-07-10 15:11:57 +0530

    Update 6_Longest Palindromic Substring[Do it without DP].cpp

--- DSA/Strings/Medium/6_Longest Palindromic Substring[Do it without DP].cpp
+++ DSA/Strings/Medium/6_Longest Palindromic Substring[Do it without DP].cpp
@@ -1,4 +1,13 @@
-bool solve(string &s, int i, int j)
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+    bool solve(string &s, int i, int j)
 {
     while (i < j)
     {
