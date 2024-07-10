7a687991f8f8f4ba555512144db8e3eb0b6a2196
Shreyansh Singhal <103640320+Shrey123ansh@users.noreply.github.com>
2024-07-10 16:12:17 +0530

    Update 6_Merge Intervals.cpp

--- DSA/Greedy/Medium/6_Merge Intervals.cpp
+++ DSA/Greedy/Medium/6_Merge Intervals.cpp
@@ -1,4 +1,15 @@
-vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr)
+Example 1:
+
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+Example 2:
+
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+    
+    vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr)
 {
     int n = arr.size(); // size of the array
 
