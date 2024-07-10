1e197ce1b123be3a62c3a5784f8fb958c816915b
Shreyansh Singhal <103640320+Shrey123ansh@users.noreply.github.com>
2024-07-10 16:10:26 +0530

    Update 4_Minimum number of platforms required for a railway.cpp

--- DSA/Greedy/Medium/4_Minimum number of platforms required for a railway.cpp
+++ DSA/Greedy/Medium/4_Minimum number of platforms required for a railway.cpp
@@ -1,4 +1,14 @@
-int findPlatform(int arr[], int dep[], int n)
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [2,3,0,1,4]
+Output: 2
+    
+    int findPlatform(int arr[], int dep[], int n)
 {
     // Your code here
     int ans = 1; // final value
