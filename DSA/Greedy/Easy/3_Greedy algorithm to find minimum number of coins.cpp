b5d0968df45a116bf2a50543c543274c8f889364
Shreyansh Singhal <103640320+Shrey123ansh@users.noreply.github.com>
2024-07-10 16:08:10 +0530

    Update 3_Greedy algorithm to find minimum number of coins.cpp

--- DSA/Greedy/Easy/3_Greedy algorithm to find minimum number of coins.cpp
+++ DSA/Greedy/Easy/3_Greedy algorithm to find minimum number of coins.cpp
@@ -1,4 +1,11 @@
+Input: coins[] = {25, 10, 5}, V = 30
+Output: Minimum 2 coins required We can use one coin of 25 cents and one of 5 cents 
+
+Input: coins[] = {9, 6, 5, 1}, V = 11
+Output: Minimum 2 coins required We can use one coin of 6 cents and 1 coin of 5 cents
+
 int minCoins(int coins[], int m, int V)
+
 {
     if (V == 0)
         return 0;
