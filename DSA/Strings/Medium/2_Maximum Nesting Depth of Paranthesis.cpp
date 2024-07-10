05d109b051c0935a7d941eef4e00346ec0dc275c
Shreyansh Singhal <103640320+Shrey123ansh@users.noreply.github.com>
2024-07-10 15:00:24 +0530

    Update 2_Maximum Nesting Depth of Paranthesis.cpp

--- DSA/Strings/Medium/2_Maximum Nesting Depth of Paranthesis.cpp
+++ DSA/Strings/Medium/2_Maximum Nesting Depth of Paranthesis.cpp
@@ -1,3 +1,22 @@
+// Example 1:
+
+// Input: s = "(1+(2*3)+((8)/4))+1"
+
+// Output: 3
+
+// Explanation:
+
+// Digit 8 is inside of 3 nested parentheses in the string.
+
+// Example 2:
+
+// Input: s = "(1)+((2))+(((3)))"
+
+// Output: 3
+
+// Explanation:
+
+// Digit 3 is inside of 3 nested parentheses in the string.
 int maxDepth(string s)
 {
     int maxi = INT_MIN;
