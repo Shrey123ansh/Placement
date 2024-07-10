7aceb847a9e78d309938a0f4c4691b3aa90a65ee
Shreyansh Singhal <103640320+Shrey123ansh@users.noreply.github.com>
2024-07-10 16:12:37 +0530

    Update 8_Non-overlapping Intervals.cpp

--- DSA/Greedy/Medium/8_Non-overlapping Intervals.cpp
+++ DSA/Greedy/Medium/8_Non-overlapping Intervals.cpp
@@ -1,4 +1,20 @@
-int eraseOverlapIntervals(vector<vector<int>> &intervals)
+Example 1:
+
+Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+Output: 1
+Explanation: [1,3] can be removed and the rest of the intervals are non-overlapping.
+Example 2:
+
+Input: intervals = [[1,2],[1,2],[1,2]]
+Output: 2
+Explanation: You need to remove two [1,2] to make the rest of the intervals non-overlapping.
+Example 3:
+
+Input: intervals = [[1,2],[2,3]]
+Output: 0
+Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
+    
+    int eraseOverlapIntervals(vector<vector<int>> &intervals)
 {
     int ans = 0, end = -1e5;
     sort(intervals.begin(), intervals.end());
