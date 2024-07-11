c73e4291ed6fcfdc3beb2e78a76439355a66cbef
Shreyansh Singhal <103640320+Shrey123ansh@users.noreply.github.com>
2024-07-11 13:50:55 +0530

    Update 2_Minimum days to make M bouquets.cpp

--- DSA/Binary Search/Hard/2_Minimum days to make M bouquets.cpp
+++ DSA/Binary Search/Hard/2_Minimum days to make M bouquets.cpp
@@ -1,4 +1,19 @@
-bool possible(vector<int> &arr, int day, int m, int k)
+Example 1:
+
+Input: bloomDay = [1,10,3,10,2], m = 3, k = 1
+Output: 3
+Explanation: Let us see what happened in the first three days. x means flower bloomed and _ means flower did not bloom in the garden.
+We need 3 bouquets each should contain 1 flower.
+After day 1: [x, _, _, _, _]   // we can only make one bouquet.
+After day 2: [x, _, _, _, x]   // we can only make two bouquets.
+After day 3: [x, _, x, _, x]   // we can make 3 bouquets. The answer is 3.
+Example 2:
+
+Input: bloomDay = [1,10,3,10,2], m = 3, k = 2
+Output: -1
+Explanation: We need 3 bouquets each has 2 flowers, that means we need 6 flowers. We only have 5 flowers so it is impossible to get the needed bouquets and we return -1.
+    
+    bool possible(vector<int> &arr, int day, int m, int k)
 {
     int n = arr.size(); // size of the array
     int cnt = 0;
