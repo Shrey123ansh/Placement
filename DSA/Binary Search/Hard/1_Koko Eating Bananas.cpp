fe2a1fe8a7cfd37165f98a05c419a1acd5d5b1a7
Shreyansh Singhal <103640320+Shrey123ansh@users.noreply.github.com>
2024-07-11 13:50:09 +0530

    Update 1_Koko Eating Bananas.cpp

--- DSA/Binary Search/Hard/1_Koko Eating Bananas.cpp
+++ DSA/Binary Search/Hard/1_Koko Eating Bananas.cpp
@@ -1,4 +1,17 @@
-int findMax(vector<int> &v)
+Example 1:
+
+Input: piles = [3,6,7,11], h = 8
+Output: 4
+Example 2:
+
+Input: piles = [30,11,23,4,20], h = 5
+Output: 30
+Example 3:
+
+Input: piles = [30,11,23,4,20], h = 6
+Output: 23
+    
+    int findMax(vector<int> &v)
 {
     int maxi = INT_MIN;
     int n = v.size();
