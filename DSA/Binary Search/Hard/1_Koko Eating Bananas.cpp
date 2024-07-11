ec4f560f6fad4cf9e257cb83d945a652ecb81b43
Shreyansh Singhal <103640320+Shrey123ansh@users.noreply.github.com>
2024-07-11 13:53:25 +0530

    Update 1_Koko Eating Bananas.cpp

--- DSA/Binary Search/Hard/1_Koko Eating Bananas.cpp
+++ DSA/Binary Search/Hard/1_Koko Eating Bananas.cpp
@@ -1,14 +1,14 @@
 Example 1:
 
-Input: piles = [3,6,7,11], h = 8
+Input: piles = [3,6,7,11], hours = 8
 Output: 4
 Example 2:
 
-Input: piles = [30,11,23,4,20], h = 5
+Input: piles = [30,11,23,4,20], hours = 5
 Output: 30
 Example 3:
 
-Input: piles = [30,11,23,4,20], h = 6
+Input: piles = [30,11,23,4,20], hours = 6
 Output: 23
     
     int findMax(vector<int> &v)
