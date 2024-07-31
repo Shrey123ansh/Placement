bd60782625ff29a45f5f0d2a8cf0416cee4b78da
Shreyansh Singhal <103640320+Shrey123ansh@users.noreply.github.com>
2024-08-01 00:46:35 +0530

    Update 8_Maximum point you can obtain from cards.cpp

--- DSA/Sliding Windows & Two pointers/Medium/8_Maximum point you can obtain from cards.cpp
+++ DSA/Sliding Windows & Two pointers/Medium/8_Maximum point you can obtain from cards.cpp
@@ -1,17 +1,18 @@
 Example 1:
 
-Input: s = "abcabc"
-Output: 10
-Explanation: The substrings containing at least one occurrence of the characters a, b and c are "abc", "abca", "abcab", "abcabc", "bca", "bcab", "bcabc", "cab", "cabc" and "abc" (again). 
+Input: cardPoints = [1,2,3,4,5,6,1], k = 3
+Output: 12
+Explanation: After the first step, your score will always be 1. However, choosing the rightmost card first will maximize your total score. The optimal strategy is to take the three cards on the right, giving a final score of 1 + 6 + 5 = 12.
 Example 2:
 
-Input: s = "aaacb"
-Output: 3
-Explanation: The substrings containing at least one occurrence of the characters a, b and c are "aaacb", "aacb" and "acb". 
+Input: cardPoints = [2,2,2], k = 2
+Output: 4
+Explanation: Regardless of which two cards you take, your score will always be 4.
 Example 3:
 
-Input: s = "abc"
-Output: 1
+Input: cardPoints = [9,7,7,9,7,7,9], k = 7
+Output: 55
+Explanation: You have to take all the cards. Your score is the sum of points of all cards.
 
 int maxScore(vector<int> &cardPoints, int k)
 {
