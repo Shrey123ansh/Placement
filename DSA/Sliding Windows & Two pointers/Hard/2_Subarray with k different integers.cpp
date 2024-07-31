56703f0c50d24892c40e4d89cd8be9d286a85079
Shreyansh Singhal <103640320+Shrey123ansh@users.noreply.github.com>
2024-08-01 00:50:21 +0530

    Update 2_Subarray with k different integers.cpp

--- DSA/Sliding Windows & Two pointers/Hard/2_Subarray with k different integers.cpp
+++ DSA/Sliding Windows & Two pointers/Hard/2_Subarray with k different integers.cpp
@@ -1,4 +1,15 @@
-int subarraysWithKDistinct(vector<int> &nums, int k)
+Example 1:
+
+Input: nums = [1,2,1,2,3], k = 2
+Output: 7
+Explanation: Subarrays formed with exactly 2 different integers: [1,2], [2,1], [1,2], [2,3], [1,2,1], [2,1,2], [1,2,1,2]
+Example 2:
+
+Input: nums = [1,2,1,3,4], k = 3
+Output: 3
+Explanation: Subarrays formed with exactly 3 different integers: [1,2,1,3], [2,1,3], [1,3,4].
+    
+    int subarraysWithKDistinct(vector<int> &nums, int k)
 {
     int sub_with_max_element_k = subarray_with_atmost_k(nums, k);
     int reduced_sub_with_max_k = subarray_with_atmost_k(nums, k - 1);
