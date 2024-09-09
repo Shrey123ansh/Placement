b75516daf5a7b5ba399252ef7317329e471ac503
Shreyansh Singhal <103640320+Shrey123ansh@users.noreply.github.com>
2024-09-10 00:54:57 +0530

    Update 3_Fruit Into Baskets.cpp

--- DSA/Sliding Windows & Two pointers/Medium/3_Fruit Into Baskets.cpp
+++ DSA/Sliding Windows & Two pointers/Medium/3_Fruit Into Baskets.cpp
@@ -3,8 +3,7 @@ Output: 3
 Explanation: We can pick one fruit from all three trees. Please note that the type of fruits is same in the 1st and 3rd baskets.
 Input: arr[] = [3, 1, 2, 2, 2, 2]
 Output: 5
-Explanation: It's optimal to pick from the last 5 trees. Please note that we do not pick the first basket as we would have to stop at thrid tree which would result in only 2 fruits collected.
-   
+Explanation: It's optimal to pick from the last 5 trees. Please note that we do not pick the first basket as we would have to stop at thrid tree which would result in only 2 fruits collected. 
 int totalFruits(int N, vector<int> &fruits)
 {
     int maxlen = 0;
