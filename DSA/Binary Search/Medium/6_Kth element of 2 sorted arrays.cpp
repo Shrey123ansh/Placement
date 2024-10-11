8de0fc105c05d50fb2abbee63a56bfbe10e80645
Shreyansh Singhal <103640320+Shrey123ansh@users.noreply.github.com>
2024-10-11 13:02:34 +0530

    Update 6_Kth element of 2 sorted arrays.cpp

--- DSA/Binary Search/Medium/6_Kth element of 2 sorted arrays.cpp
+++ DSA/Binary Search/Medium/6_Kth element of 2 sorted arrays.cpp
@@ -1,23 +1,77 @@
-int missingK(vector < int > vec, int n, int k) {
-    for (int i = 0; i < n; i++) {
-        if (vec[i] <= k) k++; //shifting k
-        else break;
+int kthElement(vector<int> &a, vector<int>& b, int m, int n, int k) {
+    vector<int> arr3;
+
+    //apply the merge step:
+    int i = 0, j = 0;
+    while (i < m && j < n) {
+        if (a[i] < b[j]) arr3.push_back(a[i++]);
+        else arr3.push_back(b[j++]);
     }
-    return k;
+
+    //copy the left-out elements:
+    while (i < m) arr3.push_back(a[i++]);
+    while (j < n) arr3.push_back(b[j++]);
+    return arr3[k - 1];
 }
 
-
-int missingK(vector < int > vec, int n, int k) {
-    int low = 0, high = n - 1;
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        int missing = vec[mid] - (mid + 1);
-        if (missing < k) {
-            low = mid + 1;
+int kthElement(vector<int> &a, vector<int>& b, int m, int n, int k) {
+    int ele = -1;
+    int cnt = 0; //counter
+    //apply the merge step:
+    int i = 0, j = 0;
+    while (i < m && j < n) {
+        if (a[i] < b[j]) {
+            if (cnt == k - 1) ele = a[i];
+            cnt++;
+            i++;
         }
         else {
-            high = mid - 1;
+            if (cnt == k - 1) ele = b[j];
+            cnt++;
+            j++;
         }
     }
-    return k + high + 1;
+
+    //copy the left-out elements:
+    while (i < m) {
+        if (cnt == k - 1) ele = a[i];
+        cnt++;
+        i++;
+    }
+    while (j < n) {
+        if (cnt == k - 1) ele = b[j];
+        cnt++;
+        j++;
+    }
+    return ele;
+}
+
+int kthElement(vector<int> &a, vector<int>& b, int m, int n, int k) {
+    if (m > n) return kthElement(b, a, n, m, k);
+
+    int left = k; //length of left half
+
+    //apply binary search:
+    int low = max(0, k - n), high = min(k, m);
+    while (low <= high) {
+        int mid1 = (low + high) >> 1;
+        int mid2 = left - mid1;
+        //calculate l1, l2, r1 and r2;
+        int l1 = INT_MIN, l2 = INT_MIN;
+        int r1 = INT_MAX, r2 = INT_MAX;
+        if (mid1 < m) r1 = a[mid1];
+        if (mid2 < n) r2 = b[mid2];
+        if (mid1 - 1 >= 0) l1 = a[mid1 - 1];
+        if (mid2 - 1 >= 0) l2 = b[mid2 - 1];
+
+        if (l1 <= r2 && l2 <= r1) {
+            return max(l1, l2);
+        }
+
+        //eliminate the halves:
+        else if (l1 > r2) high = mid1 - 1;
+        else low = mid1 + 1;
+    }
+    return 0; //dummy statement
+
 }
